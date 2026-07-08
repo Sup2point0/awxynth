af5ea60cd353e78e69956c4017d74c5f0eb31f5a
Sup#2.0 <sup2point0@gmail.com>
2026-07-08 21:32:02 +0100

    add landing overlay

---
+++ src/parts/overlay.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+
+import { synth } from "#scripts/synth";
+
+import { onMount } from "svelte";
+import { blur } from "svelte/transition";
+
+
+let open = $state(true);
+
+onMount(() => {
+  setTimeout(close, 1000);
+});
+
+
+function close() {
+  open = false;
+  synth.init();
+}
+
+</script>
+
+
+{#if open}
+  <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+  <aside
+    class="overlay init"
+    onclick={close}
+    onkeydown={close}
+    transition:blur={{ duration: 1000 }}
+  >
+    <h1> awxynth </h1>
+  </aside>
+{/if}
+
+
+<style lang="scss">
+
+aside {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to right in oklch, #ff0090, #9090f1);
+}
+
+h1 {
+  @include font-fun;
+  font-size: 250%;
+  color: white;
+}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,20 @@
 <script lang="ts">
 
+import { synth } from "#scripts/synth";
+
+import { bind_keybinds } from "./keybinds";
+
+import Overlay from "#parts/overlay.svelte";
+
 import { onMount } from "svelte";
 
 
-let ctx: AudioContext;
-let osc: OscillatorNode;
-
-let freq = $state(440);
-
-
 onMount(() => {
-  let real = new Float32Array(4096);
-  let imag = new Float32Array(4096);
-
-  for (let x = 1; x < 4096; x += 2) {
-    imag[x] = 4.0 / (Math.PI * x);
-  }
-
-  ctx = new AudioContext();
-  let wavetable = ctx.createPeriodicWave(real, imag);
-  osc = ctx.createOscillator();
-  osc.setPeriodicWave(wavetable);
-  osc.connect(ctx.destination);
-  osc.start(0);
-});
-
-$effect(() => {
-  freq;
-  if (osc) osc.frequency.value = freq;
+  bind_keybinds();
 });
 
 </script>
 
 
-<button onclick={() => ctx.resume()}>
-  Play
-</button>
 
-<input type="range" bind:value={freq} />
+<Overlay />
