a66c237429c3d291c58c76cf7992241a3fe9173b
Sup#2.0 <sup2point0@gmail.com>
2026-07-19 12:45:16 +0100

    add dummy `+` button

---
+++ src/parts/add.svelte
@@ -0,0 +1,30 @@
+<!-- @component Add -->
+
+<script lang="ts">
+
+</script>
+
+
+<button>
+  +
+</button>
+
+
+<style lang="scss">
+
+button {
+  padding: 1rem;
+  @include font-ui;
+  color: rgb(white, 20%);
+  font-size: 200%;
+  background: none;
+  border: none;
+  outline: none;
+
+  &:hover {
+    color: $col-prot;
+    background: rgb(white, 2%);
+  }
+}
+
+</style>
--- src/parts/index.js
+++ src/parts/index.js
@@ -1,4 +1,5 @@
 export { default as Overlay } from "./overlay.svelte";
 export { default as Keyboard } from "./keyboard.svelte";
 
+export { default as Add } from "./add.svelte";
 export { default as GraphEditor } from "./graph-editor";
