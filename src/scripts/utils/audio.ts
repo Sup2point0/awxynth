12b4c1111508fd11fbbbae032c9634a23e10b306
Sup#2.0 <sup2point0@gmail.com>
2026-07-25 13:58:44 +0100

    add `downsample()` util

--- src/scripts/utils/audio.ts
+++ src/scripts/utils/audio.ts
@@ -1,4 +1,4 @@
-import type { Amplitude } from "#scripts/types";
+import type { int, Amplitude } from "#scripts/types";
 
 
 /**
@@ -7,4 +7,27 @@ import type { Amplitude } from "#scripts/types";
 export function clip(data: Amplitude[]): Amplitude[]
 {
   return data.map(x => Math.min(1.0, Math.max(-1.0, x)));
+}
+
+
+/**
+ * Lower the resolution of `data` by interpolating chunks of size `rate`.
+ * 
+ * For instance, `downsample(_, 2)` halves the number of points by averaging each pair of adjacent points.
+ */
+export function downsample(data: ArrayLike<number>, rate: int): number[]
+{
+  let out = [];
+  let sum = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    sum += data[i];
+
+    if ((i + 1) % rate === 0) {
+      out.push(sum / rate);
+      sum = 0;
+    }
+  }
+
+  return out;
 }
--- tests/unit/utils/audio.test.ts
+++ tests/unit/utils/audio.test.ts
@@ -1,24 +1,85 @@
-import { clip } from "#scripts/utils";
+import { clip, downsample } from "#scripts/utils";
 
 
 describe("audio", () =>
 {
   describe("clip()", () =>
   {
-    test("identity", () => {
+    test("handles empty", () => {
+      expect(clip([])).toEqual([]);
+    });
+
+    test("handles identity", () => {
       expect(clip([0.5, 0.5, 0.5])).toEqual([0.5, 0.5, 0.5]);
     });
+
     test("clips down", () => {
       expect(clip([1.5, 2.0, 50.0])).toEqual([1.0, 1.0, 1.0]);
     });
+
     test("clips up", () => {
       expect(clip([-1.5, -2.0, -50.0])).toEqual([-1.0, -1.0, -1.0]);
     });
-    test("extreme", () => {
+
+    test("handles extremes", () => {
       expect(clip([-1.0, 0.0, 1.0])).toEqual([-1.0, 0.0, 1.0]);
     });
-    test("empty", () => {
-      expect(clip([])).toEqual([]);
+  });
+
+  const sample1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const sample2 = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
+
+  describe("downsample()", () =>
+  {
+    test("scales length / low", () => {
+      for (let rate = 1; rate < 4; rate++) {
+        expect(downsample(sample1, rate).length).toBe(sample1.length / rate);
+        expect(downsample(sample2, rate).length).toBe(sample2.length / rate);
+      }
+    });
+
+    test("scales length / high", () => {
+      const sample = Array.from({ length: 64_000 }, _ => Math.random());
+      
+      for (
+        let rate = 1;
+        rate < 256;
+        rate += 1 + Math.round(Math.random())
+      ) {
+        expect(downsample(sample, rate).length).toBeGreaterThanOrEqual(Math.floor(sample.length / rate))
+        expect(downsample(sample, rate).length).toBeLessThanOrEqual(Math.ceil(sample.length / rate))
+      }
+    });
+
+    test("handles empty", () => {
+      for (let rate = 1; rate < 10; rate++) {
+        expect(downsample([], rate)).toEqual([]);
+      }
+    });
+
+    test("handles identity", () => {
+      expect(downsample(sample1, 1)).toEqual(sample1);
+      expect(downsample(sample2, 1)).toEqual(sample2);
+    });
+
+    test("x2", () => {
+      expect(downsample(sample1, 2)).toEqual([0.5, 2.5, 4.5, 6.5, 8.5, 10.5]);
+      expect(downsample(sample2, 2)).toEqual([0, 1, 2, 3, 4, 5]);
+    });
+
+    test("x3", () => {
+      expect(downsample(sample1, 3)).toEqual([1, 4, 7, 10]);
+      expect(downsample(sample2, 3)).toEqual([1/3, 5/3, 10/3, 14/3]);
+    });
+
+    test("x4", () => {
+      expect(downsample(sample1, 4)).toEqual([1.5, 5.5, 9.5]);
+      expect(downsample(sample2, 4)).toEqual([0.5, 2.5, 4.5]);
+    });
+
+    test("x2 x2", () => {
+      expect(downsample(downsample(sample1, 2), 2)).toEqual(downsample(sample1, 4));
+      expect(downsample(downsample(sample2, 2), 2)).toEqual(downsample(sample2, 4));
     });
   });
 });
