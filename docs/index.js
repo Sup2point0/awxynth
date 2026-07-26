18d22a6c2ca2444e4ad1a0277fc2bf48ef4e9842
Sup#2.0 <sup2point0@gmail.com>
2026-07-26 18:16:48 +0100

    move help pages to `docs/`

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+export { default as Credits } from "./credits.md"
+
+export { default as Quickstart } from "./README.md"
+export { default as FAQ } from "./faq.md";
+export { default as Glossary } from "./glossary.md";
