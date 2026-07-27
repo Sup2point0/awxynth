556501f63a79a38342abe6984264841d008d5b51
Sup#2.0 <sup2point0@gmail.com>
2026-07-27 20:18:42 +0100

    revert distortion presets to use `f(x)` instead of `f(t)`

--- src/scripts/const/presets/distortions.ts
+++ src/scripts/const/presets/distortions.ts
@@ -9,61 +9,62 @@ export const distortions = presets(
     {
       title: "Linear",
       latex: [
-        ltx `f\left(t\right)=At+k`,
+        ltx `f\left(x\right) = A\left(Dx+k\right)`,
         { latex: ltx `A = 1.25`, sliderBounds: { min: 1 } },
+        { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `k = 0`, sliderBounds: { min: 0, max: 1 } },
       ],
     },
     {
       title: "Arctan",
       latex: [
-        ltx `f\left(t\right)=A\frac{\tan^{-1}\left(kDt\right)}{\tan^{-1}\left(k\right)}`,
+        ltx `f\left(x\right) = A\frac{\tan^{-1}\left(kDx\right)}{\tan^{-1}\left(k\right)}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 2`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 2`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Hyperbolic",
       latex: [
-        ltx `f\left(t\right)=\frac{A\tanh\left(kDt\right)}{\tanh\left(k\right)}`,
+        ltx `f\left(x\right) = \frac{A\tanh\left(kDx\right)}{\tanh\left(k\right)}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 2`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 2`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Reciprocal (concave)",
       latex: [
-        ltx `f\left(t\right)=A\frac{\left(1-\frac{1}{1+kDt}\right)}{1-\frac{1}{1+k}}`,
+        ltx `f\left(x\right) = A\frac{\left(1-\frac{1}{1+kDx}\right)}{1-\frac{1}{1+k}}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 2`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 2`, sliderBounds: { min: 0.001 } },
       ],
     },
     // TODO reciprocal convex
     {
       title: "Exponential (concave)",
       latex: [
-        ltx `f\left(t\right)=A\left(Dt\right)^{\frac{1}{k}}`,
+        ltx `f\left(x\right) = A\left(Dx\right)^{\frac{1}{k}}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 2`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 2`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Exponential (convex)",
       latex: [
-        ltx `f\left(t\right)=A\left(Dt\right)^{k}`,
+        ltx `f\left(x\right) = A\left(Dx\right)^{k}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 2`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 2`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Circular (concave)",
       latex: [
-        ltx `f\left(t\right)=A\sqrt{1-\left(Dt-1\right)^{2}}`,
+        ltx `f\left(x\right) = A\sqrt{1-\left(Dx-1\right)^{2}}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
       ],
@@ -71,7 +72,7 @@ export const distortions = presets(
     {
       title: "Circular (convex)",
       latex: [
-        ltx `f\left(t\right)=A\left(1-\sqrt{1-\left(Dt\right)^{2}}\right)`,
+        ltx `f\left(x\right) = A\left(1-\sqrt{1-\left(Dx\right)^{2}}\right)`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
       ],
@@ -81,64 +82,64 @@ export const distortions = presets(
     {
       title: "Logarithmic (concave)",
       latex: [
-        ltx `f\left(t\right)=A\left(\frac{1}{k}\ln\left(Dt\left(1-e^{-k}\right)+e^{-k}\right)+1\right)`,
+        ltx `f\left(x\right) = A\left(\frac{1}{k}\ln\left(Dx\left(1-e^{-k}\right)+e^{-k}\right)+1\right)`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 2`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 2`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Logarithmic (convex)",
       latex: [
-        ltx `f\left(t\right)=A\frac{e^{k\left(Dt-1\right)}-e^{-k}}{1-e^{-k}}`,
+        ltx `f\left(x\right) = A\frac{e^{k\left(Dx-1\right)}-e^{-k}}{1-e^{-k}}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 2`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 2`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Sine",
       latex: [
-        ltx `f\left(t\right)=A\sin\left(\frac{\pi}{2}\min\left(Dt,1\right)\right)^{k}`,
+        ltx `f\left(x\right) = A\sin\left(\frac{\pi}{2}\min\left(Dx,1\right)\right)^{k}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 1`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 1`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Arcsine",
       latex: [
-        ltx `f\left(t\right)=A\left(\frac{2}{\pi}\sin^{-1}\left(Dt-1\right)+1\right)^{k}`,
+        ltx `f\left(x\right) = A\left(\frac{2}{\pi}\sin^{-1}\left(Dx-1\right)+1\right)^{k}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 1`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 1`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Sigmoid",
       latex: [
-        ltx `f\left(t\right)=A\frac{1}{1+e^{-\left(10k\left(Dt-\frac{1}{2}\right)\right)}}`,
+        ltx `f\left(x\right) = A\frac{1}{1+e^{-\left(10k\left(Dx-\frac{1}{2}\right)\right)}}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 1`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 1`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Inverse Sigmoid",
       latex: [
-        ltx `f\left(t\right)=A\left(\frac{1}{10k}\ln\left(\frac{1}{1-Dt}-1\right)+\frac{1}{2}\right)`,
+        ltx `f\left(x\right) = A\left(\frac{1}{10k}\ln\left(\frac{1}{1-Dx}-1\right)+\frac{1}{2}\right)`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 1`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 1`, sliderBounds: { min: 0.001 } },
       ],
     },
     {
       title: "Arcsecant",
       latex: [
-        ltx `f\left(t\right)=A\frac{\sec^{-1}\left(Dkt+1\right)}{\sec^{-1}\left(k+1\right)}`,
+        ltx `f\left(x\right) = A\frac{\sec^{-1}\left(kDx+1\right)}{\sec^{-1}\left(k+1\right)}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 1`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 1`, sliderBounds: { min: 0.001 } },
       ],
     },
   ],
@@ -146,7 +147,7 @@ export const distortions = presets(
     {
       title: "Sine Bounce",
       latex: [
-        ltx `f\left(t\right)=A\left|\sin\left(D\pi t\right)\right|^{k}`,
+        ltx `f\left(x\right) = A\left|\sin\left(\pi Dx\right)\right|^{k}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `k = 1`, sliderBounds: { min: 0, max: 1 } },
@@ -155,19 +156,37 @@ export const distortions = presets(
     {
       title: "Saw Slice",
       latex: [
-        ltx `f\left(t\right)=A\operatorname{mod}\left(Dt+k,1\right)`,
+        ltx `f\left(x\right) = A\operatorname{mod}\left(Dx+k,1\right)`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 2`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 0`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 0`, sliderBounds: { min: 0.001 } },
+      ],
+    },
+    {
+      title: "Sine Slice",
+      latex: [
+        ltx `f\left(x\right) = A\left|\sin\left(\pi k\left(Dx\right)^{2}\right)\right|`,
+        { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
+        { latex: ltx `D = 2`, sliderBounds: { min: 1 } },
+        { latex: ltx `k = 1`, sliderBounds: { min: 1 } },
       ],
     },
     {
       title: "Sine-Saw",
       latex: [
-        ltx `f\left(t\right)=At\left|\sin\left(8\pi Dt\right)\right|^{k}`,
+        ltx `f\left(x\right) = Ax\left|\sin\left(8\pi Dx\right)\right|^{k}`,
         { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
         { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
-        { latex: ltx `k = 1`, sliderBounds: { min: "0.001" } },
+        { latex: ltx `k = 1`, sliderBounds: { min: 0.001 } },
+      ],
+    },
+    {
+      title: "Rope Spool",
+      latex: [
+        ltx `f\left(x\right) = A\left(Dx+0.1x\sin\left(2\pi kDx\right)\right)`,
+        { latex: ltx `A = 1`, sliderBounds: { min: 1 } },
+        { latex: ltx `D = 1`, sliderBounds: { min: 1 } },
+        { latex: ltx `k = 5`, sliderBounds: { min: 0 } },
       ],
     },
   ],
