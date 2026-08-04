9c907287b37acbd8bc5eb29ae5206dfe637885ff
Sup#2.0 <sup2point0@gmail.com>
2026-08-04 10:00:01 +0100

    implement persisted user preferences

--- src/scripts/stores/index.ts
+++ src/scripts/stores/index.ts
@@ -1,1 +1,2 @@
 export { nav_state, OverlayPage, Docs } from "./nav";
+export { prefs, UserPrefs } from "./prefs";
---
+++ src/scripts/stores/prefs.ts
@@ -0,0 +1,22 @@
+import { persisted } from "svelte-persisted-store";
+
+import type { int } from "#scripts/types";
+
+
+export interface UserPrefs
+{
+  ADVANCED_FEATURES: boolean
+
+  /** How many discrete samples points are taken of a continuous shaper function. */
+  SHAPER_SAMPLE_RES: int
+
+  MATHS_TERMS: boolean
+}
+
+
+export const prefs = persisted<UserPrefs>("awx.prefs",
+{
+  ADVANCED_FEATURES: false,
+  SHAPER_SAMPLE_RES: 512,
+  MATHS_TERMS: false,
+});
